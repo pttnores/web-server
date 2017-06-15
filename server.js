c7a01162295cc9e898ee5e492c8fb32fbcd30de8
José Nores <pttnores@gmail.com>
2017-06-15 17:18:39 -0300

    First steps

---
+++ server.js
@@ -0,0 +1,22 @@
+var PORT = 3000;
+var express = require('express');
+var app = express();
+
+var middleware = require('./middleware.js');
+
+app.use(middleware.logger);
+
+app.get("/about", middleware.requireAuthentication, function (req, res) {
+  res.send("<html><body><h1>Hello about us!</h1></body><html>");
+});
+
+
+app.get("/oldhome", function (req, res) {
+  res.send("Hello Express home!");
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.listen(PORT, function () {
+  console.log("Express server started on port " + PORT);
+});
